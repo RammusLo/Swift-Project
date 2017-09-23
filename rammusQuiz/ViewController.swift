95846e21b700d6445f3cb11dda0f4f9add728f4d
UCode <ucode@UCode-MBA13-T11X.fios-router.home>
2017-09-23 13:08:48 -0700

    UI Outlets added

--- rammusQuiz/ViewController.swift
+++ rammusQuiz/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  rammusQuiz
-//
+//  TriviaGo!
 //  Created by UCode on 9/9/17.
 //
 //
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var optionA: UIButton!
+    @IBOutlet var optionB: UIButton!
+    @IBOutlet var optionC: UIButton!
+    @IBOutlet var optionD: UIButton!
+    
+    var questions = ["What year was Declaration of Independence Signed?", "Where is North Korea in terms of China?", "How long does it take for light from the sun to reach Earth?"]
+    var answers = [
+                    ["1767", "1776", "1492", "1849"],
+                    ["North", "South", "East", "West"],
+                    ["8 minutes", "8 seconds", "8 hours", "instant"]
+                  ]
+    var questionIndex = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[questionIndex]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func checkAnswer (sender: AnyObject) {
+        
+        
+    }
 }
 
